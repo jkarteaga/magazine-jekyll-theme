4dfe63f004cdcd7833253e48cf93422fc10d4ee1
Matthew Scaperoth <mscapero@gwu.edu>
2015-08-25 12:27:26 -0400

    update

---
+++ js/loader.js
@@ -0,0 +1,10 @@
+/**
+ * loader support coutesy of https://ihatetomatoes.net/create-custom-preloading-screen/
+ */
+
+$(document).ready(function() {
+	
+		console.log('loaded')
+		$('body').addClass('loaded');
+	
+});
